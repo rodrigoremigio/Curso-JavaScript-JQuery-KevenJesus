45bf0211bc1bdea42559fe81d96bd7116ea0af2d
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-28 16:04:18 +0000

    Aula 5 - getElementsByTagName

--- js/cursojavascript.js
+++ js/cursojavascript.js
@@ -12,3 +12,10 @@ console.log(document.getElementsByClassName("titulo")[1]);
 
 console.log(document.getElementsByClassName("subtitulo")); //Busca a collection
 console.log(document.getElementsByClassName("subtitulo")[0]); // Busca somente a posição 0 do array
+
+/*---------------------------------*\
+	Aula 5 - getElementsByTagName
+\*---------------------------------*/
+
+console.log(document.getElementsByTagName("button"));
+console.log(document.getElementsByTagName("button")[0].innerHTML);
