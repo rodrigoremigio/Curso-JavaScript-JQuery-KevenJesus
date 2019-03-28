754938490c9d663ac58490605883e3c4c057452c
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-28 16:39:05 +0000

    Aula 7 - querySelectorAll

--- js/cursojavascript.js
+++ js/cursojavascript.js
@@ -27,3 +27,13 @@ console.log(document.getElementsByTagName("button")[0].innerHTML);
 // No caso de classes e tags, o querySlector não faz o ARRAY. Ele acessa só o primeiro elemento.
 console.log(document.querySelector("#time-grande"));
 console.log(document.querySelector(".subtitulo"));
+
+/*---------------------------------*\
+	 Aula 7 - querySelectorAll
+\*---------------------------------*/
+
+// Acesso a todas as classes "filho"
+console.log(document.querySelectorAll(".filho"));
+
+// Acessa somente as classes "filho" que são submissas à div de id "pai".
+console.log(document.getElementById("pai").querySelectorAll(".filho"));
